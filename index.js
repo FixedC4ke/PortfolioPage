6e92040fd6441ce53cb03fea1ffab5269f586df0
FixedC4ke <drmixe@gmail.com>
2021-07-16 23:29:16 +0600

    destruct

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var serviceItems = document.getElementsByClassName("services-item");
 
 for (var i = 0; i<serviceItems.length; i++){
-serviceItems[i].addEventListener("mouseover", (event)=>{
-    if (event.target===event.currentTarget){
-        event.target.classList.add("service-selected");
-        event.target.children["readmore"].classList.add('service-selected-btn');
+serviceItems[i].addEventListener("mouseover", ({target, currentTarget})=>{
+    if (target===currentTarget){
+        target.classList.add("service-selected");
+        target.children["readmore"].classList.add('service-selected-btn');
     }
 });
-serviceItems[i].addEventListener("mouseleave", (event)=>{
-    if (event.target===event.currentTarget){
-        event.target.classList.remove("service-selected");
-        event.target.children["readmore"].classList.remove('service-selected-btn');
+serviceItems[i].addEventListener("mouseleave", ({target, currentTarget})=>{
+    if (target===currentTarget){
+        target.classList.remove("service-selected");
+        target.children["readmore"].classList.remove('service-selected-btn');
     }
 });
 }
